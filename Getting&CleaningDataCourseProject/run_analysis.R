a4996f670cf73f899ad9b117e07e8d9ac0a140e2
Sam Jacob <Sam.Jacob@synechron.com>
2016-11-22 21:32:21 +0530

    Getting & Cleaning Data | Project -v2

--- Getting&CleaningDataCourseProject/run_analysis.R
+++ Getting&CleaningDataCourseProject/run_analysis.R
@@ -1,10 +1,13 @@
 #run_analysis.R
 library("data.table")
+library("sqldf")
+library("dplyr")
 
-read_files <- function(directory)
+#reading all training & test files into memory
+read_files <- function(directory,pattern="")
 {
   setwd(directory)
-  for(f in dir())
+  for(f in dir(pattern = pattern))
   {
     assign(f,fread(f))
     assign(strsplit(f,".txt")[[1]], as.data.frame(get(f)), envir = .GlobalEnv)
@@ -21,21 +24,60 @@ system.time(read_files(dir2))
 tr <- ls()[grepl("train",ls())]
 ts <- ls()[grepl("test",ls())]
 
+#merging all training and test files into *merge files
+mapply(function(x,y) {assign(paste(strsplit(x,"train")[1],"merge",sep=""),rbind(get(x),get(y)),envir = .GlobalEnv)},as.list(tr),as.list(ts))
+
+#saving dimensions of all training, test and merge objects and storing them in data.frame for easy view
+tm <- ls()[grepl("merge",ls())]
+
 tr_sizes <- sapply(tr,function(x) {return(object.size(get(x)))})
 ts_sizes <- sapply(ts,function(x) {return(object.size(get(x)))})
+tm_sizes <- sapply(tm,function(x) {return(object.size(get(x)))})
 
 tr_ncol <- sapply(tr,function(x) {return(ncol(get(x)))})
 ts_ncol <- sapply(ts,function(x) {return(ncol(get(x)))})
+tm_ncol <- sapply(tm,function(x) {return(ncol(get(x)))})
 
 tr_nrow <- sapply(tr,function(x) {return(nrow(get(x)))})
 ts_nrow <- sapply(ts,function(x) {return(nrow(get(x)))})
+tm_nrow <- sapply(tm,function(x) {return(nrow(get(x)))})
+
+df_objects <- data.frame(Tr_Size=tr_sizes,Ts_Size=ts_sizes,Tm_Size=tm_sizes,Tr_Ncols=tr_ncol,Ts_Ncols=ts_ncol,Tm_Ncols=tm_ncol,Tr_Nrows=tr_nrow,Ts_Nrows=ts_nrow,Tm_Nrows=tm_nrow)
+
+dir3 <- "C:/Users/sam.jacob/Documents/data/UCI HAR Dataset/train"
+dir4 <- "C:/Users/sam.jacob/Documents/data/UCI HAR Dataset/test"
+
+system.time(read_files(dir3,"train"))
+system.time(read_files(dir4,"test"))
 
 
-i <- 1
-for(df in tr)
-{
-  assign(paste(strsplit(df,"train")[1],"_merge",sep = ""),rbind(get(df),ts[i]))
-  i <- i+1
-}
+test <- cbind(subject_test,y_test,X_test)
+names(test)[1:2] <- c("Subject","labels")
 
-mapply(rbind,tr,ts)
+train <- cbind(subject_train,y_train,X_train)
+names(train)[1:2] <- c("Subject","labels")
+
+setwd("C:/Users/sam.jacob/Documents/data/UCI HAR Dataset")
+features <- fread("features.txt")
+features <- as.data.frame(features)
+labels <- fread("activity_labels.txt")
+labels <- as.data.frame(labels)
+
+names(train)[3:563] <- features$V2
+names(test)[3:563] <- features$V2
+
+total <- rbind(train,test)
+
+t <- data.frame(labels = total$labels)
+
+l <- sqldf("select labels.V2 from t,labels where t.labels=labels.V1")
+total$labels <- as.character(l$V2)
+
+cols <- grepl("mean",names(total),ignore.case = T) | grepl("std",names(total),ignore.case = T)
+cols[1:2] <- TRUE
+selected <- total[,cols]
+names(selected) <- sapply(names(selected),function(x) {gsub("\\- | \\( | \\) | \\,","",as.character(x))})
+
+selected2 <- tbl_df(selected)
+by_person <- group_by(selected2,labels,Subject)
+summarize(by_person,mean(by_person$tBodyAcc-mean()-X))
