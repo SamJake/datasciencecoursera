522fb897700a6c583fd5c9a8aa124caaea5fca4a
Sam Jake <samjulian88@gmail.com>
2016-11-23 23:51:45 +0530

    Getting & Cleaning Data | Course Project -v4

--- Getting&CleaningDataCourseProject/run_analysis.R
+++ Getting&CleaningDataCourseProject/run_analysis.R
@@ -15,8 +15,8 @@ read_files <- function(directory,pattern="")
   }
 }
 
-dir1 <- "C:/Users/sam.jacob/Documents/data/UCI HAR Dataset/train/Inertial Signals"
-dir2 <- "C:/Users/sam.jacob/Documents/data/UCI HAR Dataset/test/Inertial Signals"
+dir1 <- "C:/Users/Sam/Downloads/UCI HAR Dataset/train/Inertial Signals"
+dir2 <- "C:/Users/Sam/Downloads/UCI HAR Dataset/test/Inertial Signals"
 
 system.time(read_files(dir1))
 system.time(read_files(dir2))
@@ -44,8 +44,8 @@ tm_nrow <- sapply(tm,function(x) {return(nrow(get(x)))})
 
 df_objects <- data.frame(Tr_Size=tr_sizes,Ts_Size=ts_sizes,Tm_Size=tm_sizes,Tr_Ncols=tr_ncol,Ts_Ncols=ts_ncol,Tm_Ncols=tm_ncol,Tr_Nrows=tr_nrow,Ts_Nrows=ts_nrow,Tm_Nrows=tm_nrow)
 
-dir3 <- "C:/Users/sam.jacob/Documents/data/UCI HAR Dataset/train"
-dir4 <- "C:/Users/sam.jacob/Documents/data/UCI HAR Dataset/test"
+dir3 <- "C:/Users/Sam/Downloads/UCI HAR Dataset/train"
+dir4 <- "C:/Users/Sam/Downloads/UCI HAR Dataset/test"
 
 system.time(read_files(dir3,"train"))
 system.time(read_files(dir4,"test"))
@@ -57,11 +57,14 @@ names(test)[1:2] <- c("Subject","labels")
 train <- cbind(subject_train,y_train,X_train)
 names(train)[1:2] <- c("Subject","labels")
 
-setwd("C:/Users/sam.jacob/Documents/data/UCI HAR Dataset")
+setwd("C:/Users/Sam/Downloads/UCI HAR Dataset")
 features <- fread("features.txt")
 features <- as.data.frame(features)
 labels <- fread("activity_labels.txt")
 labels <- as.data.frame(labels)
+
+#Removing -,(,) and comma from features
+features$V2 <- gsub("[-(),]","",features$V2)
 
 names(train)[3:563] <- features$V2
 names(test)[3:563] <- features$V2
@@ -76,8 +79,9 @@ total$labels <- as.character(l$V2)
 cols <- grepl("mean",names(total),ignore.case = T) | grepl("std",names(total),ignore.case = T)
 cols[1:2] <- TRUE
 selected <- total[,cols]
-names(selected) <- sapply(names(selected),function(x) {gsub("\\- | \\( | \\) | \\,","",as.character(x))})
+names(selected) 
 
 selected2 <- tbl_df(selected)
 by_person <- group_by(selected2,labels,Subject)
-summarize(by_person,mean(by_person$tBodyAcc-mean()-X))
+summarize(by_person,sapply(names(by_person)[3:88],function(x) {paste0("mean(",x,")")}))
+sapply(names(by_person)[3:88])
